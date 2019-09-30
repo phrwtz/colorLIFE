9894f86cef83797f2b22882046ca4b56c74f22b5
Paul Horwitz <phorwitz@concord.org>
2019-09-30 17:06:37 -0400

    Added fill for squares.

--- searches.js
+++ searches.js
@@ -1,97 +1,90 @@
 //Search the board looking for "squares" – four tiles with the same fill color that occupy the corners of a square.
-function findSquares(box) {
-    var boxX = parseInt(box.id.split("_")[0]),
-        boxY = parseInt(box.id.split("_")[1]),
-        color = box.getAttribute("fill");
-    var upNear = findNearest(box, color, "up"),
-        downNear = findNearest(box, color, "down"),
-        leftNear = findNearest(box, color, "left"),
-        rightNear = findNearest(box, color, "right");
-    var upColor = findColor(box, 0, -upNear),
-        downColor = findColor(box, 0, downNear),
-        leftColor = findColor(box, -leftNear, 0),
-        rightColor = findColor(box, rightNear, 0);
-    console.log("up color = " + upColor + ", down color = " + downColor + ", left color = " + leftColor + " right color = " + rightColor);
+function findSquares(b) {
+    var x = parseInt(b.id.split("_")[0]),
+        y = parseInt(b.id.split("_")[1]),
+        c = getColor(x, y);
+    var dxArray = horiziontalMatches(x, y, c),
+        dyArray = verticalMatches(x, y, c),
+        squares = cornersMatch(x, y, dxArray, dyArray);
+    console.log(squares);
+    for (var i = 0; i < squares.length; i++) {
+        fillSquare(squares[i], c);
+    }
 }
 
-function findColor(box, dx, dy) {
-    var boxX = parseInt(box.id.split("_")[0]),
-        boxY = parseInt(box.id.split("_")[1]),
-        color = "white";
-    if ((Math.abs(dx) != size + 1) && (Math.abs(dy) != size + 1)) {
-        var id = (boxX + dx).toString() + "_" + (boxY + dy).toString();
-        color = document.getElementById(id).getAttribute("fill");
+function cornersMatch(x, y, dxArray, dyArray) {
+    var c = getColor(x, y),
+        dx,
+        dy,
+        squares = [];
+    for (i = 0; i < dxArray.length; i++) {
+        dx = dxArray[i];
+        for (j = 0; j < dyArray.length; j++) {
+            dy = dyArray[j];
+            if ((dx == dy) || (dx == -dy)) {
+                if (getColor(x + dx, y + dy) == c) {
+                    squares.push([x, y, dx, dy]);
+                }
+            }
+        }
     }
-    return color;
+    return squares;
 }
 
-    function findNearest(box, color, direction) { //Returns the distance of the nearest box with the fill <color> in the direction <direction> from box. If no such box exists, returns the dimention of the board plus 1.
-        var boxX = parseInt(box.id.split("_")[0]),
-            boxY = parseInt(box.id.split("_")[1]),
-            index,
-            returnIndex = size + 1;
-        if (direction === "down") {
-            index = 1;
-            searchY = boxY + index;
-            while ((searchY >= 0) && (searchY < size - 1)) {
-                searchId = boxX.toString() + "_" + (boxY + index).toString();
-                searchBox = document.getElementById(searchId);
-                searchColor = searchBox.getAttribute("fill");
-                if (searchColor === color) {
-                    return index;
-                } else {
-                    index++;
-                    searchY = boxY + index;
-                }
+function fillSquare(square, color) {
+    var x = square[0],
+        y = square[1],
+        dx = square[2],
+        dy = square[3],
+        c = fillColor(color);
+    (dx > 0 ? sgnX = 1 : sgnX = -1);
+    (dy > 0 ? sgnY = 1 : sgnY = -1);
+    for (var i = 0; i < sgnX * dx + 1; i++) {
+        for (var j = 0; j < sgnY * dy + 1; j++) {
+            if (!corner(i, j, dx, dy)) {
+                getBox(x + sgnX * i, y + sgnY * j).setAttribute("fill", c);
             }
-            return returnIndex;
-        }
-        if (direction === "up") {
-            index = 1;
-            searchY = boxY - index;
-            while ((searchY >= 0) && (searchY < size - 1)) {
-                searchId = boxX.toString() + "_" + (boxY - index).toString();
-                searchBox = document.getElementById(searchId);
-                searchColor = searchBox.getAttribute("fill");
-                if (searchColor === color) {
-                    return index;
-                } else {
-                    index++;
-                    searchY = boxY - index;
-                }
-            }
-            return returnIndex;
-        }
-        if (direction === "right") {
-            index = 1;
-            searchX = boxX + index;
-            while ((searchX >= 0) && (searchX < size - 1)) {
-                searchId = (boxX + index).toString() + "_" + boxY.toString();
-                searchBox = document.getElementById(searchId);
-                searchColor = searchBox.getAttribute("fill");
-                if (searchColor === color) {
-                    return index;
-                } else {
-                    index++;
-                    searchX = boxX + index;
-                }
-            }
-            return returnIndex;
-        }
-        if (direction === "left") {
-            index = 1;
-            searchX = boxX - index;
-            while ((searchX >= 0) && (searchX < size - 1)) {
-                searchId = (boxX - index).toString() + "_" + boxY.toString();
-                searchBox = document.getElementById(searchId);
-                searchColor = searchBox.getAttribute("fill");
-                if (searchColor === color) {
-                    return index;
-                } else {
-                    index++;
-                    searchX = boxX - index;
-                }
-            }
-            return returnIndex;
         }
     }
+}
+
+function corner(i, j, dx, dy) {
+    return ((i == 0 && j == 0) || (i == Math.abs(dx) && j == 0) || (i == 0 && j == Math.abs(dy)) || (i == Math.abs(dx) && j == Math.abs(dy)))
+}
+
+function fillColor(color) {
+    if (color === "red") {
+        return "pink";
+    } else if (color === "blue") {
+        return "paleturquoise";
+    }
+}
+
+function horiziontalMatches(x, y, color) {
+    var arr = [];
+    for (var i = 0; i < size; i++) {
+        if ((getColor(i, y) === color) && (i != x)) {
+            arr.push(i - x);
+        }
+    }
+    return arr;
+}
+
+function verticalMatches(x, y, color) {
+    var arr = [];
+    for (var i = 0; i < size; i++) {
+        if ((getColor(x, i) === color) && (i != y)) {
+            arr.push(i - y);
+        }
+    }
+    return arr;
+}
+
+function getBox(x, y) {
+    var id = x.toString() + "_" + y.toString();
+    return document.getElementById(id);
+}
+
+function getColor(x, y) {
+    return getBox(x, y).getAttribute("fill");
+}
